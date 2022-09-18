8b3df4b5707b98077df6a67076165121d1453fb5
Anders Lyager Kaae <anders.kaae@gmail.com>
2022-09-18 23:10:01 +0200

    first commit

---
+++ src/components/TheMain.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="content-container">
+      <h1 class="title">BD Law</h1>
+      <h1 class="tagline">LET'S GET LEGAL</h1>
+    </div>
+</template>
+
+<style scoped>
+.content-container{
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+}
+.title{
+  font-family: "StickyPops", Helvetica, Arial;
+  font-size: 200px;
+  color: white;
+  text-align: center;
+}
+.tagline{
+  font-family: "StickyPops", Helvetica, Arial;
+  font-size: 100px;
+  color: white;
+  padding-top: 100px;
+  text-align: center;
+}
+</style>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="navContainer">
+        <li @click="increment('TEST')">Oplysninger</li>
+        <li>Datapolitik</li>
+        <li>Betingelser</li>
+        <li>Kontakt</li>
+</div>
+</template>
+
+<style scoped>
+.navContainer {
+    left: 0;
+    width: 100vw;
+    position: absolute;
+    top: 10px;
+    text-align: center;
+    z-index: 10;
+}
+li {
+    display: inline;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    padding-right: 70px;
+    color: rgb(208, 23, 222);
+    font-weight: 800;
+    cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+      console.log(this.count)
+    }
+  }
+}
+</script>
